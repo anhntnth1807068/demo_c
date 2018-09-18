5f30a9c23dc2705f6dfa82757bb26104a85b06bf
Ngo Anh <anhntnth1807068@fpt.edu.vn>
2018-09-18 18:11:33 +0700

    Khoi tao project

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//
+//int main() {
+//    int mynumber[5]={1,2,3,4,5};
+//    int i;
+//    for(i=0;i<5;i++){
+//        printf("phantuthula%d va %d\n",i,mynumber[i]);
+//    }
+//    return 0;
+//{
+
+
+
+//
+//#include <stdio.h>
+//int main(){
+//
+//int sizeofarray =10;
+//int arraynumber[sizeofarray];
+//int i;
+//for (i=0;i<sizeofarray;i++){
+//    printf("giatriphantucuaindex%d:\n",i);
+//    scanf("%d",&arraynumber[i]);
+//}
+//for(i=0;i<sizeofarray;i++){
+//    scanf("giatriphantucuaindex%dva%d\n",i,arraynumber[i]);
+//
+//}
+//    return 0;
+//}
+//
+
+#include <stdio.h>
+inc
+int main(){
